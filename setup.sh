cf722f409c7f5476cb0335dbebfd6740784e03c7
Diego Magalhaes <dmlmagal@gmail.com>
2020-06-06 19:19:43 -0300

    Create a setup bash for personalized project
    This commit removes the sample project and adds a sh script
    to customize a django project from scratch

---
+++ setup.sh
@@ -0,0 +1,27 @@
+
+PROJECT_NAME=$1
+
+if [ ! $1 ]; then
+    echo Error: You must provide the project name.
+else
+    pipenv run django-admin startproject ${PROJECT_NAME} .
+    sed -i "s/myproject/${PROJECT_NAME}/g" ./myenv.env ./docker-compose.yml
+    sed -i 's|project_path|'$(pwd)/$PROJECT_NAME'|g' ./Caddyfile
+    sed -i '71,81 d' $(pwd)/${PROJECT_NAME}/settings.py
+    tee -a $(pwd)/${PROJECT_NAME}/settings.py << END
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'POST': 5432,
+    },
+}
+END
+fi
