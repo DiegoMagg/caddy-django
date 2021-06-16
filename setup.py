8cc9cba107b33603470a11e35f518a1c3bbc318e
Diego Magalhaes <dmlmagal@gmail.com>
2021-06-16 15:05:49 -0300

    add first version of python setup

--- setup.py
+++ setup.py
@@ -8,14 +8,11 @@ FILES_TO_REPLACE_PROJECT_NAME = ['myenv.env', 'docker-compose.yml', 'settings.py
 
 
 def setup(project_name):
-    # subprocess.run(['pipenv', 'run', 'django-admin', 'startproject', project_name])
+    subprocess.run(['pipenv', 'run', 'django-admin', 'startproject', project_name])
     for dname, dirs, files in os.walk(PROJECT_PATH):
         for i in [f'{dname}/{f}' for f in files if f in FILES_TO_REPLACE_PROJECT_NAME]:
-            with open(i, 'rw') as file:
-                content = file.read()
-                content.replace('myproject', project_name)
-                file.write(content)
-    pass
+            with open(i) as to_read, open(i, 'w') as to_write:
+                to_write.write(to_read.read().replace('myproject', project_name))
 
 
 if __name__ == '__main__':
