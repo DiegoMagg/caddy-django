204e2792946b4b8d1f3ae7f3241b137f5ce396eb
Diego Magalhaes <dmlmagal@gmail.com>
2021-06-16 15:30:23 -0300

    add first version of python setup

--- setup.py
+++ setup.py
@@ -4,15 +4,45 @@ import pathlib
 import os
 
 PROJECT_PATH = str(pathlib.Path().absolute())
-FILES_TO_REPLACE_PROJECT_NAME = ['myenv.env', 'docker-compose.yml', 'settings.py']
 
 
 def setup(project_name):
-    subprocess.run(['pipenv', 'run', 'django-admin', 'startproject', project_name])
+    params = ['pipenv', 'run', 'django-admin', 'startproject', project_name, '.']
+    subprocess.run(params)
+    replace_project_name_in_files(project_name)
+
+
+def replace_project_name_in_files(project_name):
     for dname, dirs, files in os.walk(PROJECT_PATH):
-        for i in [f'{dname}/{f}' for f in files if f in FILES_TO_REPLACE_PROJECT_NAME]:
-            with open(i) as to_read, open(i, 'w') as to_write:
-                to_write.write(to_read.read().replace('myproject', project_name))
+        for file in files:
+            if file in ['myenv.env', 'docker-compose.yml', 'settings.py']:
+                with open(f'{dname}/{file}') as old_file:
+                    new_content = old_file.read().replace('myproject', project_name)
+                with open(f'{dname}/{file}', 'w') as new_file:
+                    new_file.write(new_content)
+
+
+def replace_default_database_settings(project_name):
+    settings_path = f'{PROJECT_PATH}/{project_name}/settings.py'
+    default = '''
+        {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    '''
+    new_conf = '''
+        {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
+            'POST': 5432,
+        }
+    '''
+    with open(settings_path) as default_settings:
+        with open(settings_path) as settings:
+            settings.write(default_settings.read().replace(default, new_conf))
 
 
 if __name__ == '__main__':
