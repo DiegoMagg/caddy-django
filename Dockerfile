3f35219a7f0ff54970209b7bb983f78b0658a54e
Diego Magalhaes <dmlmagal@gmail.com>
2021-06-18 13:02:10 -0300

    replace setup.sh for setup.py

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,22 @@
 FROM python:3.9.0-alpine3.12
 
+LABEL maintainer = "Diego Magalhães <dmlmagal@gmail.com>"
+
 ENV LC_ALL C.UTF-8
 ENV LANG C.UTF-8
 ENV PYTHONUNBUFFERED 1
-LABEL maintainer = "Diego Magalhães <dmlmagal@gmail.com>"
 
 WORKDIR /app
 ADD Pipfile /app/Pipfile
 ADD Pipfile.lock /app/Pipfile.lock
 
 RUN pip3 install --no-cache-dir --upgrade pipenv pip && rm -rf /root/.cache/*
-
 RUN apk add --no-cache --virtual .build-deps \
     build-base \
     musl-dev \
     linux-headers \
-    postgresql-dev \
-  && pipenv install --three \
+    && apk add postgresql-dev --no-cache \
+    && pipenv install --three \
   && rm -rf /root/.cache/* \
   && apk del .build-deps
 
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from sys import argv
 import subprocess
 import pathlib
-import os
+from yaml import dump
+from re import sub
+
 
 PROJECT_PATH = str(pathlib.Path().absolute())
 
@@ -9,40 +11,96 @@ PROJECT_PATH = str(pathlib.Path().absolute())
 def setup(project_name):
     params = ['pipenv', 'run', 'django-admin', 'startproject', project_name, '.']
     subprocess.run(params)
-    replace_project_name_in_files(project_name)
+    create_docker_compose_yaml(project_name)
+    replace_default_database_settings(project_name)
+    create_initial_dot_env(project_name)
 
 
-def replace_project_name_in_files(project_name):
-    for dname, dirs, files in os.walk(PROJECT_PATH):
-        for file in files:
-            if file in ['myenv.env', 'docker-compose.yml', 'settings.py']:
-                with open(f'{dname}/{file}') as old_file:
-                    new_content = old_file.read().replace('myproject', project_name)
-                with open(f'{dname}/{file}', 'w') as new_file:
-                    new_file.write(new_content)
+def create_docker_compose_yaml(project_name):
+    yaml_dict = {
+        "version": "3.7",
+        "services": {
+            "web": {
+                "container_name": f"{project_name}-web",
+                "build": {"context": ".", "dockerfile": "Dockerfile"},
+                "command": (
+                    f"pipenv run gunicorn {project_name}.wsgi"
+                    " --bind 0.0.0.0:8000"
+                ),
+                "env_file": ".env",
+                "image": "docker.teste",
+                "depends_on": ["postgres"],
+                "restart": "on-failure",
+                "volumes": [".:/app"],
+            },
+            "caddy": {
+                "image": "caddy:2.3.0-alpine",
+                "links": ["web:web"],
+                "ports": ["80:80", "443:443"],
+                "volumes": ["./Caddyfile:/etc/caddy/Caddyfile"],
+            },
+            "migrations": {
+                "build": {"context": ".", "dockerfile": "Dockerfile"},
+                "command": "pipenv run python manage.py migrate",
+                "env_file": ".env",
+                "image": "docker.teste",
+                "depends_on": ["postgres"],
+                "restart": "on-failure",
+                "volumes": [".:/app"],
+            },
+            "postgres": {
+                "container_name": f"{project_name}-postgres",
+                "image": "postgres:13.3-alpine",
+                "volumes": ["postgres_data:/var/lib/postgresql/data/"],
+                "restart": "on-failure",
+                "env_file": ".env",
+            },
+        },
+        "volumes": {"postgres_data": None},
+    }
+    with open(f'{PROJECT_PATH}/docker-compose.yaml', 'w') as yml:
+        yml.write(dump(yaml_dict, sort_keys=False, line_break='\n'))
 
 
 def replace_default_database_settings(project_name):
     settings_path = f'{PROJECT_PATH}/{project_name}/settings.py'
-    default = '''
-        {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    '''
-    new_conf = '''
-        {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRES_HOST'),
-            'POST': 5432,
-        }
-    '''
-    with open(settings_path) as default_settings:
-        with open(settings_path) as settings:
-            settings.write(default_settings.read().replace(default, new_conf))
+    path_lib_str = 'from pathlib import Path'
+    new_conf = (
+        '{\n'
+        "        'ENGINE': 'django.db.backends.postgresql',\n"
+        "        'NAME': os.environ.get('POSTGRES_DB'),\n"
+        "        'USER': os.environ.get('POSTGRES_USER'),\n"
+        "        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),\n"
+        "        'HOST': os.environ.get('POSTGRES_HOST'),\n"
+        "        'POST': 5432,\n"
+        '    },'
+    )
+    with open(settings_path, 'rt') as default_settings:
+        default_settings = default_settings.read()
+    with open(settings_path, 'wt') as settings:
+        default_settings = default_settings.replace(
+            path_lib_str,
+            f'{path_lib_str}\nimport os\n',
+        )
+        settings.write(
+            sub(
+                r"({\n\s*\'E\w+.*)\n\s*\S\w+\S*\s.*\n.*",
+                new_conf,
+                default_settings,
+            ),
+        )
+
+
+def create_initial_dot_env(project_name):
+    with open(f'{PROJECT_PATH}/.env', 'w') as envfile:
+        envfile.write((
+            f'DJANGO_SETTINGS_MODULE={project_name}.settings\n'
+            'POSTGRES_DB=postgres\n'
+            'POSTGRES_USER=postgres\n'
+            'POSTGRES_PASSWORD=postgres\n'
+            'POSTGRES_HOST=postgres\n'
+            'PYTHONPATH=.'
+        ))
 
 
 if __name__ == '__main__':
